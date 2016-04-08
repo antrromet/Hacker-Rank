a7b654a5a58b5aaa46a147d869d293faf75bf702
Antrromet <antrromet@gmail.com>
2016-04-08 12:04:52 -0700

    Added Workbook

---
+++ Algorithms/Implementation/Workbook.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Workbook {
+
+	public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int k = in.nextInt();
+        int[] t = new int[n];
+        for(int i=0;i<n;i++){
+        	t[i] = in.nextInt();
+        }
+        
+        int pageNum = 0;
+        int magicCount = 0;
+        
+        for(int i=0;i<n;i++){
+        	int x=1;
+        	int j=1;
+        	pageNum++;
+        	while(x<=t[i]){
+        		if(x == pageNum){
+        			magicCount++;
+        		}
+        		if(j == k && x<t[i]){
+        			j=0;
+        			pageNum++;
+        		}
+        		j++;
+        		x++;
+        	}
+        }
+        
+        System.out.println(magicCount);
+    }
+
+}
